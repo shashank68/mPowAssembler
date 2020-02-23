56d8cc534bd6342a01842b02133883f7e775012e
shashank68 <shashankindiamanoj@gmail.com>
2020-02-24 01:36:37 +0530

    assemnb

--- test.asm
+++ test.asm
@@ -1,8 +1,10 @@
-.data
-X: .word 5
-Y: .asciiz "Hith ^ere"
+.data 
+    X: .word 5
+.text
+main:  
+    addi 8, 0, 10
+    addi 9, 0 , 20
+    add 10, 9, 8
+    addi 4, 10, 0
+    la 5, X
 
-.text 
-    Z:
-        add 5,6,7
-        bca 31,31,Z
---
+++ uPower_sim.py
@@ -0,0 +1,167 @@
+line = "01111100001000100001001000010100"
+
+#defining the compute table.
+
+from math import pow
+
+#register table----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+register_table = []
+
+for i in range(0,32):
+    register_table.append(0)
+
+register_table[2] = -1 * int(pow(2, 66))
+register_table[3] =  1
+
+special_purpose_regs = {pc: 0}
+
+data_table = {}
+
+#required functions------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def get_decimal_value(binary_value):
+   counter = len(binary_value)-1
+   decimal = 0
+   for digit in binary_value:
+       decimal = decimal + int(digit) * pow(2, counter)
+       counter = counter - 1
+   return(int(decimal))
+
+def get_two_complement_number(binary_value):
+    initial_value = -1 * int(binary_value[0]) * pow(2, len(binary_value)-1)
+    counter = len(binary_value)-2
+    for digit in binary_value[1:]:
+        initial_value = initial_value + int(digit) * pow(2, counter)
+        counter = counter - 1
+    return(int(initial_value))
+
+def check_overflow(value):
+    if value > (int(pow(2, 63))-1):
+        binary = bin(value)
+        binary = binary[2:]
+        binary = binary[len(binary)-64:]
+        value  = get_two_complement_number(binary)
+    elif value < (-1 * int(pow(2, 63))):
+        value = value + int(pow(2, 64))
+    return value
+
+#mini instruction set-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+def add():
+    register_table[get_decimal_value(line[6:11])] = register_table[get_decimal_value(line[11:16])] + register_table[get_decimal_value(line[16:21])]
+    register_table[get_decimal_value(line[6:11])] = check_overflow(int(register_table[get_decimal_value(line[6:11])]))
+
+def subf():
+    register_table[get_decimal_value(line[6:11])] = register_table[get_decimal_value(line[16:21])] - register_table[get_decimal_value(line[11:16])]
+    register_table[get_decimal_value(line[6:11])] = check_overflow(int(register_table[get_decimal_value(line[6:11])]))
+
+def addi(called = 0):
+    if called == 0:
+        register_table[get_decimal_value(line[6:11])] = register_table[get_decimal_value(line[11:16])] + get_two_complement_number(line[16:])
+    else:
+        register_table[get_decimal_value(line[6:11])] = register_table[get_decimal_value(line[11:16])] - get_two_complement_number(line[16:])
+    register_table[get_decimal_value(line[6:11])] = check_overflow(int(register_table[get_decimal_value(line[6:11])]))
+
+def Nand():
+    register_table[get_decimal_value(line[6:11])] = ~(register_table[get_decimal_value(line[11:16])] & register_table[get_decimal_value(line[16:21])])
+
+def beq():
+    pass
+
+def load(called = 0):
+    pass
+
+def store(called = 0):
+    pass 
+
+def And():
+    register_table[get_decimal_value(line[6:11])] = (register_table[get_decimal_value(line[11:16])] & register_table[get_decimal_value(line[16:21])])
+
+def Or():
+    register_table[get_decimal_value(line[6:11])] = (register_table[get_decimal_value(line[11:16])] | register_table[get_decimal_value(line[16:21])])
+
+def SRDW():
+    get_binary = bin(register_table[get_decimal_value(line[16:21])])
+    get_binary = get_binary[2:]
+    get_binary = get_binary[len(get_binary)-7:]
+    get_integer = get_decimal_value(get_binary)
+    register_table[get_decimal_value(line[6:11])] = register_table[get_decimal_value(line[11:16])] >> get_integer
+    register_table[get_decimal_value(line[6:11])] = check_overflow(int(register_table[get_decimal_value(line[6:11])]))
+
+def SLDW():
+    get_binary = bin(register_table[get_decimal_value(line[16:21])])
+    get_binary = get_binary[2:]
+    get_binary = get_binary[len(get_binary)-7:]
+    get_integer = get_decimal_value(get_binary)
+    register_table[get_decimal_value(line[6:11])] = register_table[get_decimal_value(line[11:16])] << get_integer
+    register_table[get_decimal_value(line[6:11])] = check_overflow(int(register_table[get_decimal_value(line[6:11])]))
+
+def Cmp():
+    pass
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#dictionaries definition.
+
+XO = {266: add, 40: subf}
+X  = {476: Nand, 28: And, 444: Or, 539: SRDW, 27: SLDW, 0: Cmp}
+XS = {}
+
+get_instruction_from_pop = {14:addi}
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def compute_instruction():
+    primary_opcode_value = get_decimal_value(line[:6])
+    if primary_opcode_value in list(get_instruction_from_pop.keys()):
+        get_instruction_from_pop[primary_opcode_value]()
+    else:
+        possible_values = []
+        possible_values = [get_decimal_value(line[21:31]), get_decimal_value(line[21:30]), get_decimal_value(line[22:31])]
+        if possible_values[0] in list(X.keys()):
+            X[possible_values[0]]()
+            return
+        elif possible_values[1] in list(XS.keys()):
+            XS[possible_values[1]]()
+            return
+        elif possible_values[2] in list(XO.keys()):
+            XO[possible_values[2]]()
+            return
+        else:
+            print("Instruction Error");
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def print_register_data():
+    print("Register contents(in decimal):")
+    print()
+    for i in range(0, 31, 4):
+        for i in range(i, i+4):
+            print("Reg[",i, "]: ", register_table[i], sep ="", end = "   ")
+        print("\n")
+
+compute_instruction()
+print_register_data()
+
+def read_data_file():
+    init_data = open('initdata.txt', 'r')
+    line_list = init_data.readlines()
+    for lin in line_list:
+        l = lin.split()
+        addr = l[1]
+        type1 = l[2]
+        no_of_data = l[3]
+        if type1 == '11':
+            data_table[addr] = [l[4:], 1]
+        elif type1 == '01':
+            data_table[addr] = [l[4:], 4]
+
+print(data_table)
+
+
+
+    
+        
+
+    
+        
